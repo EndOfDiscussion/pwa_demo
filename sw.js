c1c9d6729e3166b87411ab13c74cc78acb97793c
Philipp Nopper <philipp.nopper@eod.de>
2018-11-08 13:29:08 +0100

    Add full path to cached files

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const filesToCache = [
     'https://endofdiscussion.github.io/pwa_demo/main.css',
     'https://endofdiscussion.github.io/pwa_demo/random.png',
     'https://endofdiscussion.github.io/pwa_demo/main.js',
+    'https://endofdiscussion.github.io/pwa_demo/public/',
 ]
 
 const staticCacheName = 'first-cache'
