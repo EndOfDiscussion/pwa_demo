d67b5c76fc32867b4a1f98aab38b435fcd8aef0c
Philipp Nopper <philipp.nopper@eod.de>
2018-11-08 11:58:40 +0100

    Fixing pathname error

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // caching the files in that constante
 const filesToCache = [
-    '/',
-    '/index.html',
-    '/main.css',
-    '/random.png',
-    '/main.js',
+    '',
+    'index.html',
+    'main.css',
+    'random.png',
+    'main.js',
 ]
 
 const staticCacheName = 'first-cache'
