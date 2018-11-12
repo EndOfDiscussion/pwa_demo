bc34542e08e24d22c0d10e7671d12809d909bcc5
Philipp Nopper <philipp.nopper@eod.de>
2018-11-12 13:04:51 +0100

    Add download banner for iOS

---
+++ ios_popup.js
@@ -0,0 +1,19 @@
+
+// detect if device is on iOS
+function isIos() {
+	return ['iPhone', 'iPad', 'iPod'].includes(navigator.platform);
+}
+
+// // detect if device is in standalone mode
+// const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+//
+// const showInstallMessage;
+// // display popup message
+// if (isIos() && !isInStandaloneMode()) {
+// 	this.setState({ showInstallMessage: true });
+// }
+
+if ( isIos() == true ) {
+	alert("Win!");
+	document.getElementById('banner').style.display = "block";
+}
