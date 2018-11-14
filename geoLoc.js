7f84258dd00ec50258f479d37cf107d518ffc8a8
Philipp Nopper <philipp.nopper@eod.de>
2018-11-14 17:38:00 +0100

    Improve error handling for geolocation

--- geoLoc.js
+++ geoLoc.js
@@ -1,10 +1,28 @@
 function getLocation() {
 	if( navigator.geolocation ) {
-		navigator.geolocation.getCurrentPosition(showPosition);
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
 	} else {
 		consol.log( "Geo Location not supported by browser" );
 	}
 }
+
+function showError(error) {
+	switch(error.code) {
+		case error.PERMISSION_DENIED:
+			loc.innerHTML = "User denied the request for Geolocation."
+			break;
+		case error.POSITION_UNAVAILABLE:
+			loc.innerHTML = "Location information is unavailable."
+			break;
+		case error.TIMEOUT:
+			loc.innerHTML = "The request to get user location timed out."
+			break;
+		case error.UNKNOWN_ERROR:
+			loc.innerHTML = "An unknown error occured."
+			break;
+	}
+}
+
 
 var loc = document.getElementById("loc");
 
