5dc944c0f8993a8f0c89a778ca8cd304c94ea641
Philipp Nopper <philipp.nopper@eod.de>
2018-11-16 10:58:49 +0100

    Register onesignal sw and import ours

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,2 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts('https://endofdiscussion.github.io/pwa_demo/service-worker.js');
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // checks for servic worker brwoser support
 // when the browser does support register a new service worker
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('service-worker.js').then(() => {
+	navigator.serviceWorker.register('OneSignalSDKWorker.js').then(() => {
 		console.log('[Service Worker] Successfully registered')
 	})
 }
