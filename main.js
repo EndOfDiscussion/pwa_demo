95a54d25ddf6112ffd7abe120ec425d1c654e183
Philipp Nopper <philipp.nopper@eod.de>
2018-11-19 12:19:24 +0100

    Add update script to SW

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 // checks for servic worker brwoser support
 // when the browser does support register a new service worker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('OneSignalSDKWorker.js').then(() => {
-		console.log('[Service Worker] Successfully registered')
-	})
-}
+window.isUpdateAvailable = new Promise(function(resolve, reject) {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('OneSignalSDKWorker.js')
+			.then(reg => {
+				console.log('[Service Worker] Successfully registered')
+				reg.onupdatefound = () => {
+					const installingWorker = reg.installing;
+					installingWorker.onstatechange = () => {
+						switch (installingWorker.state) {
+							case 'installed':
+								if (navigator.serviceWorker.controller) {
+									// new update available
+									resolve(true);
+								} else {
+									// no update available
+									resolve(false);
+								}
+								break;
+						}
+					};
+				};
+			})
+			.catch(err => console.error('[SW ERROR]', err));
+	}
+});
+
+
+window['isUpdateAvailable']
+	.then(isAvailable => {
+		if (isAvailable) {
+			const toast = this.toastCtrl.create({
+				message: 'New Update available! Reload the webapp to see the latest juicy changes.',
+				position: 'bottom',
+				showCloseButton: true,
+			});
+			toast.present();
+		}
+	});
